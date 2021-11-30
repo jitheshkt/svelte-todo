4dab8f83d5a0ab057f75078fd42910bb7733376b
Jithesh Kt <jithesh.kt@bridge-global.com>
2021-12-01 00:35:10 +0530

    fix runtime error on netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
